6dc806ab0dd0d46f9f000a851b0089ce85be4bc6
AspenJames <aspenjames@tqca.org>
2018-08-15 02:05:12 -0700

    setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(panic_implementation)]
+#![feature(core_intrinsics)]
+#![no_std]
+#![no_main]
+
+extern crate bootloader_precompiled;
+
+use core::intrinsics;
+use core::panic::PanicInfo;
+
+#[panic_implementation]
+#[no_mangle]
+fn panic(_info: &PanicInfo) -> ! {
+    unsafe { intrinsics::abort() }
+}
+
+#[no_mangle]
+pub fn _start() -> ! {
+    loop {}
 }
