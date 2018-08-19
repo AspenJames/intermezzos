913ec41e5a9da9a03df36e3749b4c58e0a257733
Aspen James <aspenjames@tqca.org>
2018-08-18 23:32:34 -0700

    hello world

--- src/main.rs
+++ src/main.rs
@@ -16,5 +16,30 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub fn _start() -> ! {
+    let slice = unsafe { core::slice::from_raw_parts_mut(0xb8000 as *mut u8, 4000) };
+
+    slice[0] = b'h';
+    slice[1] = 0x04;
+    slice[2] = b'e';
+    slice[3] = 0x0E;
+    slice[4] = b'l';
+    slice[5] = 0x02;
+    slice[6] = b'l';
+    slice[7] = 0x09;
+    slice[8] = b'o';
+    slice[9] = 0x0D; 
+    slice[10] = b' ';
+    slice[11] = 0x05;
+    slice[12] = b'w';
+    slice[13] = 0x04;
+    slice[14] = b'o';
+    slice[15] = 0x0E;
+    slice[16] = b'r';
+    slice[17] = 0x02;
+    slice[18] = b'l';
+    slice[19] = 0x09;
+    slice[20] = b'd';
+    slice[21] = 0x0D;
+
     loop {}
 }
